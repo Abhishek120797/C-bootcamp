822573e09be529141e24319640a5b1f9b73b273a
Abhishek jaiswal <abhi.jais1211@gmail.com>
2022-10-01 01:03:10 +0530

    uploaded

--- assignment 19-multi dimension string/Q10.c
+++ assignment 19-multi dimension string/Q10.c
@@ -2,6 +2,34 @@
 #include<string.h>
 int main()
 {
-
+    char user_pass[5][2][25];
+    char user[25],pass[25];
+    int i,j,flag=0;
+    for(i=0;i<5;i++)
+    {
+        printf("Enter %d user name : ",i+1);
+        for(j=0;j<2;j++)
+        {
+            if(j==1)
+                printf("Enter password : ");
+        gets(user_pass[i][j]);
+        }
+    }
+    printf("Enter your user name to log in: ");
+    gets(user);
+    printf("Enetr your password : ");
+    gets(pass);
+    for(i=0;i<5;i++)
+    {
+        if(strcmp(user_pass[i][0],user)==0 && strcmp(user_pass[i][1],pass)==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+        printf("<<<login succesfull");
+    else
+        printf("incorrect username password");
     return 0;
 }
--- assignment 19-multi dimension string/Q7.c
+++ assignment 19-multi dimension string/Q7.c
@@ -2,23 +2,30 @@
 #include<string.h>
 int main()
 {
-    char ip[4][25],*byte;
-    int i,num,flag;
-    printf("Enter 4 ip address : ");
+    char ip[4][25],*byte,temp[25];
+    int num,i,flag;
+    printf("enter 4 ip address\n");
     for(i=0;i<4;i++)
-      gets(ip[i]);
+        gets(ip[i]);
+    printf("list of valid ip address\n");
     for(i=0;i<4;i++)
     {
+        strcpy(temp,ip[i]);
+        byte=(strtok(temp,"."));
         flag=0;
-        byte=strtok(ip[i],".");
         while(byte!=NULL)
         {
             num=atoi(byte);
             if(num>=0 && num<=255)
+            {
                 flag++;
+            }
+            byte=strtok(NULL,".");
         }
         if(flag==4)
+        {
             printf("%s\n",ip[i]);
+        }
     }
     return 0;
 }
